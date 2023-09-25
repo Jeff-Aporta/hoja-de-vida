4859709213142fa32d1a30c0f79089ba73ec620b
Jeffrey Agudelo <jeffaporta@gmail.com>
2023-09-25 06:45:41 -0500

    2023-09-25

---
+++ views/portafolio_proyectos.jsx
@@ -0,0 +1,31 @@
+let Javascript = {
+    Juegos: {
+        proyectos:[
+            {
+                
+            }
+        ]
+    },
+    Aplicativos: {
+
+    },
+    Algoritmos: {
+
+    },
+    "Estructura de datos": {
+
+    },
+}
+
+Javascript = {
+    url: "javascript",
+    proyectos: [
+        {
+            nombre: "Juegos",
+            url: "juegos",
+            proyectos: [
+                {
+                    nombre: "Buscaminas",
+                    url: "buscaminas",
+                    
+}   
